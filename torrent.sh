a516d0072e9bdfe311c3a175e9f15f3ad604942a
Mohammed Anas <anasmohammed361@gmail.com>
2021-12-18 22:39:01 +0530

    torrent correction

--- torrent.sh
+++ torrent.sh
@@ -1,6 +1,6 @@
+#!bin/bash
 read -e -p "Torrent File :" File
 echo $File 
 transmission-daemon
-alias tsm="transmission-remote"
-tsm -a $File
-echo"typr tsm -l for info or tsm -t :file_id -r for removing"
+transmission-remote -a $File
+echo 'typr transmission-remote -l for info ||| transmission -t :file_id -r for removing'
