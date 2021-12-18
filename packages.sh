7c577e494fd8405e42606e2ad3763c435af9b251
Mohammed Anas <anasmohammed361@gmail.com>
2021-12-18 15:40:36 +0530

    first commit

---
+++ packages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git clone https://aur.archlinux.org/brave-bin.git
+cd brav*
+makepkg
+sudo pacman -U brav*.zst ||pacman -U brav*.gz
+git clone https://aur.archlinux.org/visual-studio-code-bin.git
+cd *code**
+makepkg
+sudo pacman -U *code**.zst ||pacman -U *code**.gz
+sudo pacman -Sy nodejs npm htop tmux vim starship
+echo'eval "$(starship init bash)"'
