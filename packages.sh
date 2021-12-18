b796f66a10b6b57301ad7625427c360d2fdd3286
Mohammed Anas <anasmohammed361@gmail.com>
2021-12-18 15:44:32 +0530

    added cd ~

--- packages.sh
+++ packages.sh
@@ -4,9 +4,11 @@ git clone https://aur.archlinux.org/brave-bin.git
 cd brav*
 makepkg
 sudo pacman -U brav*.zst ||pacman -U brav*.gz
+cd ~
 git clone https://aur.archlinux.org/visual-studio-code-bin.git
 cd *code**
 makepkg
 sudo pacman -U *code**.zst ||pacman -U *code**.gz
+cd ~
 sudo pacman -Sy nodejs npm htop tmux vim starship
 echo'eval "$(starship init bash)"'
