28e8d6ed0d543adfb356b134feb8f26389b3654d
Mohammed Anas <anasmohammed361@gmail.com>
2021-12-19 07:51:06 +0530

    Fixed a bug

--- torrent.sh
+++ torrent.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+echo 'Please make sure you have a permanent alias (tsm)'
 read -e -p "Torrent File :" File
 echo $File 
+wait
 transmission-daemon
+wait
 transmission-remote -a $File
+wait
 echo 'type tsm -l for info ||| tsm -t :file_id -r for removing'
