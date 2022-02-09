66cd6aaa504a6868c0a9742c8d5525cadfd3a3b7
Mohammed Anas <78010612+anasmohammed361@users.noreply.github.com>
2022-02-09 07:00:05 +0530

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
-yay -S nodejs npm htop neofetch  tmux starship visual-studio-code-bin brave-bin vim grub-customizer transmission-cli ksuperkey unzip libreoffice-fresh mongosh-bin
+yay -S nodejs npm htop neofetch  tmux starship visual-studio-code-bin brave-bin vim grub-customizer transmission-cli ksuperkey unzip wps-office mongosh-bin
